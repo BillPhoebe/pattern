53bd218adc41ef140532ae093284fa4ee6a8e49b
Phoebe <419427210@qq.com>
2018-11-23 18:21:56 +0800

    update

--- src/main/java/yyd/phoebe/Hello.java
+++ src/main/java/yyd/phoebe/Hello.java
@@ -1,7 +1,15 @@
 package yyd.phoebe;
 
+import java.time.ZonedDateTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 public class Hello {
     public static void main(String[] args) {
-
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(10);
+        scheduledExecutorService.scheduleAtFixedRate(() -> {
+            System.out.println(ZonedDateTime.now());
+        },0,2,TimeUnit.SECONDS);
     }
 }
